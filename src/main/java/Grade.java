701847eb028e0c046b7fac340e9b7eca2de16a12
OfriBash <126421560+OfriBash@users.noreply.github.com>
2024-03-17 10:59:07 +0200

    Add files via upload

---
+++ src/main/java/Grade.java
@@ -0,0 +1,34 @@
+package test;
+
+public class Grade {
+	private String apparatus ;
+	private double score;
+
+
+	public Grade(String apparatus, double score) {
+		this.apparatus = apparatus;
+		this.score = score;
+	}
+
+
+	public String getApparatus() {
+		return apparatus;
+	}
+
+
+	public void setApparatus(String apparatus) {
+		this.apparatus = apparatus;
+	}
+
+
+	public double getScore() {
+		return score;
+	}
+
+
+	public void setScore(double score) {
+		this.score = score;
+	}
+	
+}
+
